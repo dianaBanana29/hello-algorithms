995581b7377dd78ea8f2414c05399c7e9783335a
diana <dcecurskaya@gmail.com>
2022-08-14 22:59:27 +0300

    formatted HW17

--- src/telran/util/TreeSet.java
+++ src/telran/util/TreeSet.java
@@ -19,19 +19,23 @@ public class TreeSet<T> implements SortedSet<T> {
 	private Node<T> root;
 	int size;
 	Comparator<T> comp;
+
 	private Node<T> getLeastNodeFrom(Node<T> node) {
-		while(node.left != null) {
+		while (node.left != null) {
 			node = node.left;
 		}
 		return node;
 	}
+
 	private class TreeSetIterator implements Iterator<T> {
 		Node<T> current = root == null ? null : getLeastNodeFrom(root);
 		boolean flNext;
 		T prevObj;
+		Node<T> prevNode;
+
 		@Override
 		public boolean hasNext() {
-			
+
 			return current != null;
 		}
 
@@ -45,14 +49,15 @@ public class TreeSet<T> implements SortedSet<T> {
 			flNext = true;
 			return prevObj;
 		}
+
 		private void updateCurrent() {
 			current = current.right != null ? getLeastNodeFrom(current.right) : getGreaterParent(current);
-			
+
 		}
 
 		private Node<T> getGreaterParent(Node<T> node) {
-			
-			while(node.parent != null && node.parent.left != node) {
+
+			while (node.parent != null && node.parent.left != node) {
 				node = node.parent;
 			}
 			return node.parent;
@@ -66,27 +71,30 @@ public class TreeSet<T> implements SortedSet<T> {
 			TreeSet.this.remove(prevObj);
 			flNext = false;
 		}
-		}
-		
+
+	}
+
 	public TreeSet(Comparator<T> comp) {
 		this.comp = comp;
 	}
+
 	@SuppressWarnings("unchecked")
 	public TreeSet() {
-		this((Comparator<T>)Comparator.naturalOrder());
+		this((Comparator<T>) Comparator.naturalOrder());
 	}
+
 	@Override
 	public boolean add(T obj) {
 		Node<T> parent = getNodeOrParent(obj);
 		boolean res = false;
 		int compRes = 0;
 		if (parent == null || (compRes = comp.compare(obj, parent.obj)) != 0) {
-			//obj doesn't exist
+			// obj doesn't exist
 			Node<T> newNode = new Node<>(obj);
 			if (parent == null) {
-				//added first element that is the root
+				// added first element that is the root
 				root = newNode;
-			} else if(compRes > 0) {
+			} else if (compRes > 0) {
 				parent.right = newNode;
 			} else {
 				parent.left = newNode;
@@ -112,68 +120,72 @@ public class TreeSet<T> implements SortedSet<T> {
 		}
 		return parent;
 	}
+
 	@Override
 	public boolean remove(Object pattern) {
-		if(getNode(pattern) != null) {
-		removeNode(getNode(pattern));
-		size--;
-		return true;
+		if (getNode(pattern) != null) {
+			removeNode(getNode(pattern));
+			size--;
+			return true;
 		}
 		return false;
-	 }
+	}
 
 	private void removeNode(Node<T> node) {
-	if(isJunction(node)) {
-		removeJunctionNode(node);
-	}else {
-		removeNonJunctionNode(node);
+		if (isJunction(node)) {
+			removeJunctionNode(node);
+		} else {
+			removeNonJunctionNode(node);
+		}
+
 	}
-	
-	}
+
 	private boolean isJunction(Node<T> node) {
 		boolean res = false;
-	if(node.left != null && node.right != null) {
-		res = true;
-	}
+		if (node.left != null && node.right != null) {
+			res = true;
+		}
 		return res;
 	}
+
 	private void removeJunctionNode(Node<T> node) {
-	Node<T> curr = getLeastNodeFrom(node.right);
-	node.obj = curr.obj;
-	removeNonJunctionNode(curr);
+		Node<T> curr = getLeastNodeFrom(node.right);
+		node.obj = curr.obj;
+		removeNonJunctionNode(curr);
 	}
+
 	private void removeNonJunctionNode(Node<T> node) {
-	if(node.left == null && node.right == null) {
-		if(node != root) {
-	
-Node<T> child = node.left != null ? node.left : node.right;
-if( node != root) {
-	if(node == node.parent.left) {
-		node.parent.left = child;
-	} else {
-		node.parent.right = child;
-	} 
-	
-}
-else {
-	root = null;
-}
-}
-				
+		if (node.left == null && node.right == null) {
+			if (node != root) {
+
+				Node<T> child = node.left != null ? node.left : node.right;
+				if (node != root) {
+					if (node == node.parent.left) {
+						node.parent.left = child;
+					} else {
+						node.parent.right = child;
+					}
+
+				} else {
+					root = null;
+				}
+			}
+
+		}
 	}
-	}
+
 	public Node<T> getNode(Object pattern) {
 		Iterator<T> it = new TreeSetIterator();
 		Node<T> current = root;
-		while(it.hasNext()) {
+		while (it.hasNext()) {
 			if (current.obj.equals(pattern)) {
 				return current;
 			}
-		current.obj = it.next();
+			current.obj = it.next();
 		}
 		return null;
 	}
-	
+
 	@Override
 	public boolean contains(Object pattern) {
 		return getNode(pattern) != null;
@@ -181,13 +193,13 @@ else {
 
 	@Override
 	public int size() {
-		
+
 		return size;
 	}
 
 	@Override
 	public Iterator<T> iterator() {
-		
+
 		return new TreeSetIterator();
 	}
 
@@ -199,7 +211,6 @@ else {
 		return getLeastNodeFrom(root).obj;
 	}
 
-	
 	@Override
 	public T last() {
 		if (root == null) {
@@ -207,8 +218,9 @@ else {
 		}
 		return getMostNodeFrom(root).obj;
 	}
+
 	private Node<T> getMostNodeFrom(Node<T> node) {
-		while(node.right != null) {
+		while (node.right != null) {
 			node = node.right;
 		}
 		return node;
